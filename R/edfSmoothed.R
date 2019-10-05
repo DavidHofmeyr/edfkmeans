2ff9afb2e888c6d1b89a0e6d00dac8b67ec382de
DavidHofmeyr <33575492+DavidHofmeyr@users.noreply.github.com>
2019-10-05 19:51:02 +0200

    Update edfSmoothed.R

--- R/edfSmoothed.R
+++ R/edfSmoothed.R
@@ -1,4 +1,4 @@
-kmeans_edf = function (X, maxk, nstart = 10, ngrid = 30, bw = 3) 
+kmeans_edf = function (X, maxk, nstart = 10, ngrid = 30, span = 0.2) 
 {
   X <- t(t(X) - colMeans(X))
   sds <- sqrt(diag(cov(X)))
@@ -23,7 +23,7 @@ kmeans_edf = function (X, maxk, nstart = 10, ngrid = 30, bw = 3)
     clusters[k,] <- sols[[k]]$cluster
   }
   edfs <- EDFS
-  for(i in 1:ngrid) EDFS[,i] <- smoothed(EDFS[,i], bw)
+  for(i in 1:ngrid) EDFS[,i] <- smoothed(EDFS[,i], span)
   BICS <- SS[1:maxk]%*%t(1/sigs^2) + EDFS*log(n*d)
   ks <- apply(BICS, 2, fmin)
   k <- ifelse(sum(ks==1)>=(ngrid*2/3), 1, ifelse(sum(ks==maxk)>=(ngrid*2/3), maxk, which.max(sapply(2:(maxk-1), function(i) sum(ks==i)))+1))
@@ -58,7 +58,7 @@ fmin = function(x){
 
 
 
-smoothed <- function(x, hh){
+smoothed <- function(x, span){
   #fun <- function(h){
   #  den = length(x)*density(1:length(x), bw = h, n = length(x), from = 1, to = length(x))$y*h
   #  denloo = den-1/sqrt(2*pi)
@@ -66,5 +66,6 @@ smoothed <- function(x, hh){
   #  sum((x-hy)^2)
   #}
   #hh <- optimise(fun, c(.5, 10))$minimum
-  KernSmooth::locpoly(1:length(x), x, range.x = c(1, length(x)), gridsize = length(x), bandwidth = hh)$y
+  #KernSmooth::locpoly(1:length(x), x, range.x = c(1, length(x)), gridsize = length(x), bandwidth = hh)$y
+  supsmu(1:length(x), x, span = span)$y
  }
